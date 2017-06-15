4be32f776e851e8b2bc79c90be8f36b8e990a4fb
Silva Kirsimae <skirsimae@tmk.edu.ee>
2017-06-15 11:17:38 +0200

    app.js modified

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var pg = require('pg'); //include node postgres library.
 
-
-app.set('views', './views');
-app.set('view engine', 'pug');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public/css'));
 app.use(express.static('./public/img'));
-app.use(bodyParser.urlencoded({extended: true}));
+ 
+ app.set('views', './views');
+ app.set('view engine', 'pug');
+ 
+ app.use(express.static('./public/css'))
+ 
+ var Sequelize = require('sequelize');
+ var db = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/memorygame');
+ 
 
-var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/memorygame';
+  var Food = db.define('food', {
+      name: Sequelize.STRING
+  });
+ 
+  var Animal = db.define('animal', {
+      name: Sequelize.STRING
+  });
+ 
+ 
+db.sync().then(function() {
+    Food.bulkCreate([
+        {name: 'pizza'}, {name: 'egg'}, {name: 'chocolate'}, {name:'bread'}, {name:'apple'}
+      ]);
 
+    Animal.bulkCreate([
+        {name: 'sheep'}, {name:'rabbit'}, {name: 'dog'}, {name: 'chicken'}, {name: 'cow'}
+      ]);  
+  })
+ 
+ 
 //Weclome page
 app.get('/', function(req,res) { 
     res.render('welcome')
@@ -29,9 +51,7 @@ app.get('/animal', function(req,res) {
 app.get('/food', function(req,res) {
     res.render('food')
 })
-
+ 
 const server = app.listen(8080, () => {
-    console.log(`server has started ${server.address().port}`)
+      console.log(`server has started ${server.address().port}`)
 })
-
-
