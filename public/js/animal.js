8d8cf23f568538acf561710431e1fa2f2fba24c7
Stephanie Lam <stephaniewylam@hotmail.com>
2017-06-16 17:29:05 +0200

    added styling to animal pug

--- public/js/animal.js
+++ public/js/animal.js
@@ -1,12 +1,36 @@
 $(document).ready(function() {
 	$('.hide').hide()
 	$('.test').hide()
+	$('.animals').hide()
+	$('.game').hide()
+	$('.help').hide()
 })
+
+$('.start').click(function() {
+	$('.start').hide()
+	$('.instructions').hide()
+	$('.animals').fadeIn()
+	$('.game').fadeIn()
+	$('.help').show()
 
 var show = function() {
 	$('.animals').hide()
 	$('.hide').fadeIn()
 	$('.test').fadeIn()
 };
-setTimeout(show, 20000)
+setTimeout(show, 1000)
+
+})
+
+$('.help').click(function() {
+	$('.hide').hide()
+	$('.test').hide()
+	$('.animals').hide()
+	$('.game').hide()
+	$('.instructions').show()
+	$('.start').show()
+	$('.help').hide()
+})
+
+
 
